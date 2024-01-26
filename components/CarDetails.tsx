8e3b098ca2f22966e01657f99888a384aa88545d
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2024-01-25 18:46:50 -0800

    with manufacturer search working and car images working

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 import { Dialog, Transition } from "@headlessui/react";
 import { CarProps } from "@/types";
+import { generateCarImageUrl } from "@/utils";
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -40,7 +41,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="relative w-full max-w-lg max-h-[90hv] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
+                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type="button"
                     onClick={closeModal}
@@ -57,12 +58,59 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car, "angle")}
                         alt="car model"
                         fill
                         priority
                         className="object-contain"
                       ></Image>
+                    </div>
+                    <div className="flex gap-3">
+                      <div className="flex- relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={generateCarImageUrl(car, "29")}
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        ></Image>
+                      </div>
+                      <div className="flex- relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={generateCarImageUrl(car, "33")}
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        ></Image>
+                      </div>
+                      <div className="flex- relative w-full h-24 bg-primary-blue-100 rounded-lg">
+                        <Image
+                          src={generateCarImageUrl(car, "13")}
+                          alt="car model"
+                          fill
+                          priority
+                          className="object-contain"
+                        ></Image>
+                      </div>
+                    </div>
+                    <div className="flex-1 flex flex-col gap-2">
+                      <h2 className="font-semibold text-xl capitalize">
+                        {car.make} {car.model}
+                      </h2>
+                      <div className="mt-3 flex flex-wrap gap-4">
+                        {Object.entries(car).map(([key, value]) => (
+                          <div
+                            className="flex justify-between gap-5 w-full text-right"
+                            key={key}
+                          >
+                            <h4 className="text-grey capitalize">
+                              {key.split("").join("")}
+                            </h4>
+                            <p>{value}</p>
+                          </div>
+                        ))}
+                      </div>
                     </div>
                   </div>
                 </Dialog.Panel>
